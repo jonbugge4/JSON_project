666dcabc1c1bd01cd0e52fad92e5728fadb9e611
Jonathan Bugge <jonathan_bugge1@baylor.edu>
2021-10-04 17:16:41 -0500

    classwork 10/3

--- json1.py
+++ json1.py
@@ -1,6 +1,6 @@
 import json
 
-infile = open('eq_data_1_day_m1.json', 'r')
+infile = open('eq_data_30_day_m1.json', 'r')
 outfile = open('readable_eq_data.json', 'w')
 
 eqdata = json.load(infile)
@@ -15,12 +15,26 @@ longs = []
 
 for eq in list_of_eqs:
     mag = eq['properties']['mag']
-    lat = eq['geometry']['coordinates'][0]
-    longit = eq['geometry']['coordinates'][1]
+    lat = eq['geometry']['coordinates'][1]
+    longit = eq['geometry']['coordinates'][0]
     mags.append(mag)
     lats.append(lat)
     longs.append(longit)
 
-print(mags)
-print(lats)
-print(longs)
+print(mags[:5])
+print(lats[:5])
+print(longs[:5])
+# How ould you list the top 5 elements? brackets
+# :5 is grabbing the first 5 elements. You can specify a range with X:X
+
+
+from plotly.graph_objs import Scattergeo, Layout 
+from plotly import offline
+
+data = [Scattergeo(lon=longs, lat=lats)]
+
+my_layout = Layout(title = 'Global Earthquakes 1 Day')
+
+fig = {'data': data, 'layout': my_layout}
+
+offline.plot(fig, filename = 'gloalearthquakes1day.html')
