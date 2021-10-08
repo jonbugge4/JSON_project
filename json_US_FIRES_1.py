51e0333da78f0fc82c67a991854aed648fea8e25
jon <jonathan_bugge1@baylor.edu>
2021-10-08 16:45:33 -0500

    US Fires 1 and 2 done

--- json_US_FIRES_1.py
+++ json_US_FIRES_1.py
@@ -9,16 +9,17 @@ json.dump(firedata, outfile, indent = 4)
 
 list_of_fires = firedata
 
-lats, longs, bright = [], [], []
+lats, longs, bright, hover_texts = [], [], [], []
 
 for fr in list_of_fires:
     lat = fr["latitude"]
     longit = fr["longitude"]
     brightness = fr["brightness"]
-    lats.append(lat)
-    longs.append(longit)
     if brightness > 450:
         bright.append(brightness)
+        lats.append(lat)
+        longs.append(longit)
+        hover_texts.append(brightness)
 
 print(lats[:5])
 print(longs[:5])
@@ -28,9 +29,24 @@ print(bright[:5])
 from plotly.graph_objs import Scattergeo, Layout 
 from plotly import offline
 
-data = [Scattergeo(lon = longs, lat = lats)]
+#data = [Scattergeo(lon = longs, lat = lats)]
 
-my_layout = Layout(title = 'California Fires on Septemeber 1st')
+data = [{
+    'type': 'scattergeo',
+    'lon': longs,
+    'lat': lats,
+    'text': hover_texts,
+    'marker':{
+        'size': [.035*b for b in bright],
+        'color': bright, 
+        'colorscale': 'Viridis',
+        'reversescale': True,
+        'colorbar': {'title': 'Brightness'}
+
+    }
+}]
+
+my_layout = Layout(title = 'US Fires - 9/1/2020 through 9/13/2020')
 
 fig = {'data': data, 'layout': my_layout}
 
--- json_US_FIRES_2.py
+++ json_US_FIRES_2.py
@@ -1,1 +1,53 @@
 import json
+
+infile = open ('US_fires_9_14.json', 'r')
+outfile = open ('readable_fire_data_9_14.json', 'w')
+
+firedata = json.load(infile)
+
+json.dump(firedata, outfile, indent = 4)
+
+list_of_fires = firedata
+
+lats, longs, bright, hover_texts = [], [], [], []
+
+for fr in list_of_fires:
+    lat = fr["latitude"]
+    longit = fr["longitude"]
+    brightness = fr["brightness"]
+    if brightness > 450:
+        bright.append(brightness)
+        lats.append(lat)
+        longs.append(longit)
+        hover_texts.append(brightness)
+
+print(lats[:5])
+print(longs[:5])
+print(bright[:5])
+
+
+from plotly.graph_objs import Scattergeo, Layout 
+from plotly import offline
+
+#data = [Scattergeo(lon = longs, lat = lats)]
+
+data = [{
+    'type': 'scattergeo',
+    'lon': longs,
+    'lat': lats,
+    'text': hover_texts,
+    'marker':{
+        'size': [.035*b for b in bright],
+        'color': bright, 
+        'colorscale': 'Viridis',
+        'reversescale': True,
+        'colorbar': {'title': 'Brightness'}
+
+    }
+}]
+
+my_layout = Layout(title = 'US Fires - 9/14/2020 through 9/20/2020')
+
+fig = {'data': data, 'layout': my_layout}
+
+offline.plot(fig, filename = 'US_Fires_9_14.html')
